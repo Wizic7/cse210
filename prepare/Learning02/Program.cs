e3c55323e944aaf451d13febb7b63716653b60a1
Wizic7 <IsaacWizic7@gmail.com>
2024-10-01 08:41:16 -0600

    Completed Learning 2 Assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job() 
+        { 
+            _company = "Microsoft", 
+            _jobTitle = "Software Engineer",
+            _endYear = 2022, 
+            _startYear = 2019
+        };
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+        
+        Resume resume= new Resume();
+        resume._name = "Allison Rose";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        
+        resume.Display();
+
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,7 +5,11 @@ class Program
 {
        static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        
+    }
+
+    static void shoppingList(){
+                Console.WriteLine("Hello Sandbox World!");
         Console.WriteLine(" ________________________");
         Console.WriteLine("|                        |");
         Console.WriteLine("|    __|__      _____    |");
@@ -23,6 +27,27 @@ class Program
         Console.WriteLine("|________________________|");
 
         Console.WriteLine();
+
+        List<String> shoppingList = new List<String>();
+        Boolean shoping = true;
+        Console.WriteLine("Welcome to my store! Type \"done\" when you finish shopping.");
+        while(shoping)
+        {
+            Console.Write("What would you like to buy? ");
+            String order = Console.ReadLine();
+            if(order == "done")
+            {
+                shoping = false;
+            }
+            else{
+                shoppingList.Add(order);
+            }
+        }
+        Console.WriteLine("Grocery List (" + shoppingList.Count + " items)");
+        foreach(String item in shoppingList)
+        {
+            Console.WriteLine("* " + item);
+        }
     }
 
 
