695fe9125e5c9a39039d88d40b52da2aabc04ef2
Wizic7 <IsaacWizic7@gmail.com>
2024-12-07 18:40:14 -0700

    Added story class

---
+++ final/FinalProject/Story.cs
@@ -0,0 +1,13 @@
+class Story
+{
+    private List<Encounter> encounters;
+    public Story()
+    {
+        LoadTextFile();
+    }
+    
+    private void LoadTextFile()
+    {
+        //Builds the encounters and story from a prebuilt txt file
+    }
+}
